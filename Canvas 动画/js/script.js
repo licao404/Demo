6ce8de3fbace1f8ce5ff8b43cfc13bd0ebf14aea
peifan <526648262@qq.com>
2016-07-11 08:31:43 +0800

    psd to html

--- Canvas 动画/js/script.js
+++ Canvas 动画/js/script.js
@@ -1,63 +1,47 @@
-var WINDOW_WIDTH = 1024;
-var WINDOW_HEIGHT = 768;
-var RADUIS = 5;
-var MARGIN_LEFT = 250;
-var MARGIN_TOP = 100;
+var CANVAS_WIDTH = 1024;
+var CANVAS_HEIGHT = 500;
+var START_TOP = 100;
+var START_LEFT = 500;
+var RADIUS = 8;
 
-var curTime = new Date();
+window.onload = function () {
+	var Public_curTime = new Date(),
+		canvas = document.getElementById("myCanvas"),
+		context = canvas.getContext('2d');
 
-window.onload = function() {
-	var canvas = document.getElementById('myCanvas');
-	canvas.width = WINDOW_WIDTH;
-	canvas.height = WINDOW_HEIGHT;
+	canvas.width = CANVAS_WIDTH;
+	canvas.height = CANVAS_HEIGHT;
 
-	var context = canvas.getContext('2d');
-
-	setInterval(function(){
-		update(context);
-	},50)
-};
-
-function update(context) {
-	var d1 = new Date();
-	if (d1.getSeconds() !== curTime.getSeconds()) {
-		curTime = d1;
+	setInterval(function () {
 		render(context);
+		// update();
+	},50);
+
+	function render(ctx) {
+		var curtime = Public_curTime;
+		var curHours = curtime.getHours(),
+			curMins	 = curtime.getMinutes(),
+			curSecs  = curtime.getSeconds();
+
+		renderDigit(START_LEFT,START_TOP,Math.floor(curHours/10),ctx);
+
 	}
-}
-function render(ctx) {
-	
-	var hours = curTime.getHours();
-	var minutes = curTime.getMinutes();
-	var seconds = curTime.getSeconds();
-	console.log(hours);
 
-	ctx.clearRect(0,0,WINDOW_WIDTH,WINDOW_HEIGHT);
+	function renderDigit(x,y,num,ctx) {
+		var arr = digit[num];
+		for(var i = 0; i < arr.length ; i++) {
+			for(var j=0 ; j < arr[i].length ; j++){
+				if (arr[i][j] === 1) {
+					ctx.beginPath();
+					ctx.arc( x+j*2*(RADIUS+1)+(RADIUS+1) , y+i*2*(RADIUS+1)+(RADIUS+1) , RADIUS , 0 , 2*Math.PI )
+					ctx.closePath();
 
-	randerDigit(MARGIN_LEFT,MARGIN_TOP,Math.floor(hours/10),ctx);
-	randerDigit(MARGIN_LEFT + 15*(RADUIS+1),MARGIN_TOP,Math.floor(hours%10),ctx);
-	randerDigit(MARGIN_LEFT + 30*(RADUIS+1),MARGIN_TOP,10,ctx);
-	randerDigit(MARGIN_LEFT + 39*(RADUIS+1),MARGIN_TOP,Math.floor(minutes/10),ctx);
-	randerDigit(MARGIN_LEFT + 54*(RADUIS+1),MARGIN_TOP,Math.floor(minutes%10),ctx);
-	randerDigit(MARGIN_LEFT + 69*(RADUIS+1),MARGIN_TOP,10,ctx);
-	randerDigit(MARGIN_LEFT + 78*(RADUIS+1),MARGIN_TOP,Math.floor(seconds/10),ctx);
-	randerDigit(MARGIN_LEFT + 93*(RADUIS+1),MARGIN_TOP,Math.floor(seconds%10),ctx);
+					ctx.()
 
-}
-
-function randerDigit(x,y,num,ctx) {
-
-	var arr = digit[num];
-	for (var i=0;i<arr.length;i++) {
-		for (var j=0;j<arr[i].length;j++) {
-			if (arr[i][j] === 1) {
-				centerX = x + j*2*(RADUIS+1)+(RADUIS+1);
-				centerY = y + i*2*(RADUIS+1)+(RADUIS+1);
-				ctx.beginPath();
-				ctx.arc(centerX,centerY,RADUIS,0,2*Math.PI);
-
-				ctx.fill();
+				}
 			}
 		}
+		
+		
 	}
-}
+};
